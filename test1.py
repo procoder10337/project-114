34a1d7505f67ad0409c1e0397a5660ed7c527f75
procoder10337 <130889208+procoder10337@users.noreply.github.com>
2024-06-16 15:05:16 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,8 @@
+A = 716
+B = 314
+if B > A :
+   print("B is greater than A")
+elif A > B:
+   print("A is greater than B")
+elif A == B :
+   print("A and B are equal")
---
+++ test2.py
@@ -0,0 +1,6 @@
+def my_functon(fname):
+    print(fname + "Refsnes")
+    
+my_functon("Emil")
+my_functon("Tobias")
+my_functon("linus")
